4f066be0df8c1f3dc179153f3f4799cf533e0ebe
johneas10 <easton3003@gmail.com>
2016-10-07 12:10:31 +0100

    mountain getters completed

--- client/src/models/mountain.js
+++ client/src/models/mountain.js
@@ -2,11 +2,32 @@ var Mountain = function(options){
   this._id = options._id;
   this._name = options.name;
   this._height = options.height;
-  this._gridref = new GridRef(options.gridRef);
+  this._gridRef = new GridRef(options.gridRef);
   this._latLng = options.latLng;
+
   Object.defineProperty(this, "id", {
     get: function(){
-      return this._id
+      return this._id;
+    };
+  });
+  Object.defineProperty(this, "name", {
+    get: function(){
+      return this._name;
+    };
+  });
+  Object.defineProperty(this, "height", {
+    get: function(){
+      return this._height;
+    };
+  });
+  Object.defineProperty(this, "gridRef", {
+    get: function(){
+      return this._gridRef;
+    };
+  });
+  Object.defineProperty(this, "latLng", {
+    get: function(){
+      return this._latLng;
     };
   });
 };
