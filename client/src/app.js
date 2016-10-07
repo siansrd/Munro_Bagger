043fe64bb1c0af98cddc06984549534a64560967
siansrd <sianrobinsondavies@gmail.com>
2016-10-07 15:05:31 +0100

    Add the add marker function in map.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,6 @@ var UI = require('./views/UI');
 
 var app = function(){
   var ui = new UI();
-  ui.displayMap();
 }
 
 window.onload = app;
--- client/src/views/UI.js
+++ client/src/views/UI.js
@@ -1,13 +1,16 @@
 var Map = require('../models/map');
 
 var UI = function(){
-
+  this.render();
 }
 
 UI.prototype = {
   displayMap: function() {
     var mapContainer = document.getElementById('mapContainer')
     new Map(mapContainer);
+  },
+  render: function() {
+    this.displayMap();
   }
 }
 
