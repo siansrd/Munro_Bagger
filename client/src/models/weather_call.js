47615ad024ffe1d27cfd67b28082525812c5377b
johneas10 <easton3003@gmail.com>
2016-10-09 18:05:08 +0100

    weather call completed. unsure about a few requirements(mongo). wait until monday to merge

--- client/src/models/weather_call.js
+++ client/src/models/weather_call.js
@@ -1,105 +1,25 @@
-var apiKey = require("....../helpers/weather_call/api_key");
-// var munroList = require("../test_data"); ???
-//cannot require these without webpack. No progress from dummy data...
+var apiKey = require("....../helpers/weather_call/api_key"); //should we move the API key?
+var munroList = require(""); //this should come from a DB now
 
-// var munroList = [
-//   {
-//     _id: 1,
-//     name: "A'Bhuidheanach Bheag",
-//     height:  936.1,
-//     gridRef: {
-//       letters: "NN",
-//       eastings: "66069",
-//       northings: "77600"
-//     },
-//     latLng: {
-//       lat: 56.87039900,
-//       lng: -4.1988390
-//     }
-//   },
-//   {
-//     _id: 2,
-//     name: "A'Chailleach",
-//     height:  997.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "13620",
-//       northings: "71414"
-//     },
-//     latLng: {
-//       lat: 57.69378200,
-//       lng: -5.1287300
-//     }
-//   },
-//   {
-//     _id: 3,
-//     name: "A'Chailleach",
-//     height:  929.2,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "68110",
-//       northings: "04178"
-//     },
-//     latLng: {
-//       lat: 57.10956400,
-//       lng: -4.1792850
-//     }
-//   },
-//   {
-//     _id: 4,
-//     name: "A'Chraileag (A'Chralaig)",
-//     height: 1120.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "09431",
-//       northings: "14797"
-//     },
-//     latLng: {
-//       lat: 57.18424000,
-//       lng: -5.1548420
-//     }
-//   },
-//   {
-//     _id: 5,
-//     name: "A'Ghlas-bheinn",
-//     height:  918.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "00822",
-//       northings: "23105"
-//     },
-//     latLng: {
-//       lat: 57.25509000,
-//       lng: -5.3036870
-//     }
-//   }
-// ];
-var weatherObjs = [];
+var weatherObjs = []; //this should point to a DB now
 var counter = 0;
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
-  // console.log(request)
   request.open("GET", url);
   request.onload = callback;
   request.send();
-  // console.log("hello cyan")
 };
 
 var requestComplete = function(){
-  // console.log(this.status)
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var response = JSON.parse(jsonString);
-  console.log(response)
-  // console.log("hello michael")
   saveWeather(response);
-  console.log(counter)
   counter++;
   if (counter < 10){
     // setTimeout(function(){
       makeRequest(urlGenerator(), requestComplete);
-      // console.log(counter)
     // }, 1050);
     //timeout maybe not required. Put back in if we have any issues
   };
--- helpers/weather_call/files/weather_call.js
+++ helpers/weather_call/files/weather_call.js
@@ -1,104 +1,98 @@
-// var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-var apiKey = require("./api_key");
-var munroList = require("../test_data");
+// var apiKey = require("./api_key");
+// var munroList = require("../test_data");
 //cannot require these without webpack. No progress from dummy data...
 
-// var munroList = [
-//   {
-//     _id: 1,
-//     name: "A'Bhuidheanach Bheag",
-//     height:  936.1,
-//     gridRef: {
-//       letters: "NN",
-//       eastings: "66069",
-//       northings: "77600"
-//     },
-//     latLng: {
-//       lat: 56.87039900,
-//       lng: -4.1988390
-//     }
-//   },
-//   {
-//     _id: 2,
-//     name: "A'Chailleach",
-//     height:  997.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "13620",
-//       northings: "71414"
-//     },
-//     latLng: {
-//       lat: 57.69378200,
-//       lng: -5.1287300
-//     }
-//   },
-//   {
-//     _id: 3,
-//     name: "A'Chailleach",
-//     height:  929.2,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "68110",
-//       northings: "04178"
-//     },
-//     latLng: {
-//       lat: 57.10956400,
-//       lng: -4.1792850
-//     }
-//   },
-//   {
-//     _id: 4,
-//     name: "A'Chraileag (A'Chralaig)",
-//     height: 1120.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "09431",
-//       northings: "14797"
-//     },
-//     latLng: {
-//       lat: 57.18424000,
-//       lng: -5.1548420
-//     }
-//   },
-//   {
-//     _id: 5,
-//     name: "A'Ghlas-bheinn",
-//     height:  918.0,
-//     gridRef: {
-//       letters: "NH",
-//       eastings: "00822",
-//       northings: "23105"
-//     },
-//     latLng: {
-//       lat: 57.25509000,
-//       lng: -5.3036870
-//     }
-//   }
-// ];
+var munroList = [
+  {
+    _id: 1,
+    name: "A'Bhuidheanach Bheag",
+    height:  936.1,
+    gridRef: {
+      letters: "NN",
+      eastings: "66069",
+      northings: "77600"
+    },
+    latLng: {
+      lat: 56.87039900,
+      lng: -4.1988390
+    }
+  },
+  {
+    _id: 2,
+    name: "A'Chailleach",
+    height:  997.0,
+    gridRef: {
+      letters: "NH",
+      eastings: "13620",
+      northings: "71414"
+    },
+    latLng: {
+      lat: 57.69378200,
+      lng: -5.1287300
+    }
+  },
+  {
+    _id: 3,
+    name: "A'Chailleach",
+    height:  929.2,
+    gridRef: {
+      letters: "NH",
+      eastings: "68110",
+      northings: "04178"
+    },
+    latLng: {
+      lat: 57.10956400,
+      lng: -4.1792850
+    }
+  },
+  {
+    _id: 4,
+    name: "A'Chraileag (A'Chralaig)",
+    height: 1120.0,
+    gridRef: {
+      letters: "NH",
+      eastings: "09431",
+      northings: "14797"
+    },
+    latLng: {
+      lat: 57.18424000,
+      lng: -5.1548420
+    }
+  },
+  {
+    _id: 5,
+    name: "A'Ghlas-bheinn",
+    height:  918.0,
+    gridRef: {
+      letters: "NH",
+      eastings: "00822",
+      northings: "23105"
+    },
+    latLng: {
+      lat: 57.25509000,
+      lng: -5.3036870
+    }
+  }
+];
 var weatherObjs = [];
 var counter = 0;
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
-  // console.log(request)
   request.open("GET", url);
   request.onload = callback;
   request.send();
-  // console.log("hello cyan")
 };
 
 var requestComplete = function(){
-  // console.log(this.status)
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var response = JSON.parse(jsonString);
   console.log(response)
-  // console.log("hello michael")
   saveWeather(response);
   console.log(counter)
   counter++;
-  if (counter < 10){
+  if (weatherObjs.length < munroList.length){
     // setTimeout(function(){
       makeRequest(urlGenerator(), requestComplete);
       // console.log(counter)
@@ -123,6 +117,7 @@ var urlGenerator = function(){
   var lat = latLngGenerator().lat;
   var lng = latLngGenerator().lng;
   var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lng + "&appid=" + apiKey;
+  //api key hidden in other file
   return url;
 };
 
