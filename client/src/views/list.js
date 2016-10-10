a49bc2e1bca1fc3847f6fddba423cddeb63b3a2a
siansrd <sianrobinsondavies@gmail.com>
2016-10-10 17:15:24 +0100

    initial tab test

--- client/src/views/list.js
+++ client/src/views/list.js
@@ -13,11 +13,22 @@ List.prototype = {
        document.getElementById('munroList').appendChild(munro); 
     };
   },
+  // createTabs: function() {
+  //   var tabs = document.createElement('ul');
+  //   var container = document.getElementById('tabsContainer').appendChild(tabs);
+  //   var tab = document.createElement('li');
+  //   var tabLink = "<a href="#" class="tablinks" onclick="openCity(event, 'London')">London</a>"
+  //   tab.innerHTML = tabLink;
+  //   tabs.appendChild(tab);
+
+
+  // },
   render: function() {
     var newMount = new Mountains();
     newMount.all(function(mtns){  
       this.populateList(mtns);
     }.bind(this)); 
+    this.createTabs();
   }
 };
 
