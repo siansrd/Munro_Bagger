77659fbc4f6b8348679ab310cd5bd3f72f8ed7f9
johneas10 <easton3003@gmail.com>
2016-10-10 14:49:23 +0100

    weather model finished

---
+++ client/src/models/weather.js
@@ -0,0 +1,25 @@
+var Weather = function(options){
+  // this._id = options.id;
+  this._timeOfRequest = options.timeOfRequest;
+  this._forcast = options.forcast;
+
+  // Object.defineProperty(this, "id", {
+  //   get: function(){
+  //     return this._id;
+  //   }
+  // });
+
+  Object.defineProperty(this, "timeOfRequest", {
+    get: function(){
+      return this._timeOfRequest;
+    }
+  });
+
+  Object.defineProperty(this, "forcast", {
+    get: function(){
+      return this._forcast;
+    }
+  });
+}
+
+module.exports = WeatherStation;
