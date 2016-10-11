115ca299d088f11e9751eb66dede0ce18b4fbd55
Lee Lawrence <leerlawrence@gmail.com>
2016-10-11 14:03:56 +0100

    Update of list

--- client/src/views/list.js
+++ client/src/views/list.js
@@ -28,26 +28,45 @@ List.prototype = {
     tabContainer.appendChild(munroUL);
   },
 
-   populateTabSections: function(myMountains){
+  populateTabSections: function(myMountains){
     var divID="A";
     var munroDiv = document.createElement('div');
     munroDiv.setAttribute("id", divID);
-    console.log(munroDiv);
+    //console.log(munroDiv);
     document.getElementById("munroTabSections").appendChild(munroDiv);
-    console.log(munroTabSection);
+    //console.log(munroTabSection);
     var munroList = document.createElement('ul');
 
     for (var i = 0;  i < myMountains.length; i++)
     {
-    // var mountainName = myMountains[i].name;
+      var mountainName = myMountains[i].name;
+      var mountInitial = mountainName.charAt(1)
+   // console.log(mountInitial +" "+ divID);
+   if (mountInitial === divID) { 
+    var munro = document.createElement('li');
+     console.log("IDs are the same");
+     munroList.appendChild(munro); 
+     munro.innerText = myMountains[i].name;
+     munroList.appendChild(munro); 
+   }else{
+    console.log("IDs have changed from "+divID+" to "+ mountInitial);
+    divID=mountInitial;
+    var munroDiv = document.createElement('div');
+    munroDiv.setAttribute("id", divID);
 
- //      if mountainName.charAt(1) === divID {
- //       var munro = document.createElement('li');
- //       munro.innerHTML = myMountains[i].name;
- //       munroList.appendChild(munro); 
-     };
- //   };
-  },
+    var munro = document.createElement('li');
+
+    console.log("The munro is " + munro);
+    munro.innerText = myMountains[i].name;
+    munroList.appendChild(munro); 
+    div = document.getElementById('foo');
+    newlink = document.createElement('a');
+    div.appendChild(newlink);
+
+
+  };
+};
+},
 
 
 
@@ -70,7 +89,7 @@ List.prototype = {
   render: function() {
     var newMount = new Mountains();
     newMount.all(function(mtns){  
-      this.populateList(mtns);
+     // this.populateList(mtns);
       this.populateTabSections(mtns);
     }.bind(this)); 
     this.addTabs();
