1b2b1aa0bc9799329edc256f795c11e9e183f5ce
Lee Lawrence <leerlawrence@gmail.com>
2016-10-11 18:01:46 +0100

    going home

--- client/src/views/list.js
+++ client/src/views/list.js
@@ -46,12 +46,18 @@ List.prototype = {
       var munroDiv = document.createElement('div');
       munroDiv.setAttribute("id", divID);
       munroDiv.setAttribute("class", "singleItem");
-      if ( i !== 0 ){
-        munroDiv.setAttribute("style","display:none");
+      munroDiv.setAttribute("onclick","openSection(event, divID)");
+
+
+      console.log(i);
+      if ( i !== 0){
+        munroDiv.setAttribute("style","visibility:hidden");
       };
 
       for(var j = 0; j < mountains.length; j++) {
         var munro = document.createElement('li');
+        munro.setAttribute("class","singleItem")
+        munroDiv.setAttribute("margin-left", "20px");
         munro.innerText = mountains[j].name;
         munroDiv.appendChild(munro); 
       }
@@ -59,20 +65,24 @@ List.prototype = {
     }
   },
 
-  openSection: function(evt) {
-     var i, tabcontent, tablinks;
-     tabcontent = document.getElementsByClassName("tabcontent");
-     for (i = 0; i < tabcontent.length; i++) {
-       tabcontent[i].style.display = "none";
-     }
-     tablinks = document.getElementsByClassName("tablinks");
-     for (i = 0; i < tablinks.length; i++) {
-       tablinks[i].className = tablinks[i].className.replace(" active", "");
-     }
-     document.getElementById(this.innerText).style.display = "block";
-     console.log(this.innerText);
-     evt.currentTarget.className += " active";
-   },
+  openSection: function(evt,divID) {
+    console.log("the div id here is " + divID);
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+     tabcontent[i].style.display = "none";
+     console.log("set display none");
+   }
+   tablinks = document.getElementsByClassName("tablinks");
+   for (i = 0; i < tablinks.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace("active", "");
+     console.log("set display relace the active link");
+   }
+   document.getElementById(divID).style.display = "block";
+   console.log(this.innerText);
+   console.log("append active none");
+   evt.currentTarget.className += " active";
+ },
 
 
   render: function() {
