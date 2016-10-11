0a7d01d92c52e962ffac4ee64d35d832baa3d4e9
johneas10 <easton3003@gmail.com>
2016-10-11 22:02:59 +0100

    tidy up

--- client/src/models/upCase.js
+++ client/src/models/upCase.js
@@ -1,9 +1,9 @@
 var upCase = function(string){
-   var splitStr = string.toLowerCase().split(' ');
-   for (var i = 0; i < splitStr.length; i++) {
-       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+   var splitString = string.toLowerCase().split(' ');
+   for (var i = 0; i < splitString.length; i++) {
+       splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1);
    }
-   return splitStr.join(' ');
+   return splitString.join(' ');
 };
 
 module.exports = upCase;
