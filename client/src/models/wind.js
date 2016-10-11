6733558be98062e6e6d0410a2304e1a5b58734e3
johneas10 <easton3003@gmail.com>
2016-10-11 10:43:21 +0100

    WindDirection completed

--- client/src/models/wind.js
+++ client/src/models/wind.js
@@ -5,8 +5,12 @@ var Wind = function(options) {
   Object.defineProperty(this, "direction", { get: function(){ return this._direction; } });
 };
 
-Wind.prototype.windDirection = function(direction){
-
+Wind.prototype.windDirection = function(){
+  var d = this.direction;
+  var dToReturn;
+  var directions = ["North", "North North-East", "North-East", "East North-East", "East", "East South-East", "South-East", "South South-East", "South", "South South-West", "South-West", "West South-West", "West", "West North-West", "North-West", "North West-North"];
+  dToReturn = directions[Math.round(d/22.5)];
+  return dToReturn;
 };
 
 module.exports = Wind;
