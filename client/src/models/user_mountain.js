01813d418500357d32a7b3502c64cb4e841a7e45
johneas10 <easton3003@gmail.com>
2016-10-12 12:50:21 +0100

    UserMountain bug squashed

--- client/src/models/user_mountain.js
+++ client/src/models/user_mountain.js
@@ -1,7 +1,12 @@
 var UserMountain = function(options) {
   this._id = options.mtn_id;
-  this.bagged = options.bagged;
+  this._bagged = options.bagged;
+  this._dirty = false;
   Object.defineProperty(this, "id", { get: function(){ return this._id; } });
+  Object.defineProperty(this, "bagged", {
+    get: function(){ return this._bagged; },
+    set: function(value){ this._bagged = value; this._dirty = true; }
+  });
 };
 
 module.exports = UserMountain;
