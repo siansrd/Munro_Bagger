1c70c414d1fe36652ed600d388f1771c35faa2b1
Phil Crooks <pacman@crooks.me.uk>
2016-10-16 09:19:38 +0100

    utility.js now also used by models/user.js

--- client/src/models/user.js
+++ client/src/models/user.js
@@ -1,23 +1,6 @@
 var UserMountain = require('./user_mountain');
 var ApiRequest = require('./api_request');
-
-var search = function(mountains, mountainId) {
-  var mId = Number(mountainId);
-
-  var binarySearch = function(first, last) {
-    var mid = first + Math.floor((last - first) / 2);
-    var mountain = mountains[mid];
-    var numberId = Number(mountain.id);
-    if (mId === numberId) return mountain;
-    if (first === last) return undefined;
-    if (mId < numberId)
-      return binarySearch(first, mid - 1);
-    else
-      return binarySearch(mid + 1, last);
-  };
-
-  return binarySearch(0, mountains.length-1);
-};
+var search = require('../utility').mountainSearch;
 
 var User = function(userId) {
   this._id = userId;
