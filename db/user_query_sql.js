7189323f33e1e30f6974fac511ddc974fa305d2d
Phil Crooks <pacman@crooks.me.uk>
2016-10-21 08:23:04 +0100

    commiting wip

--- db/user_query_sql.js
+++ db/user_query_sql.js
@@ -1,4 +1,4 @@
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('mysql');
 
 var quote = function (str) {
   return "'" + str.toString() + "'";
@@ -6,44 +6,28 @@ var quote = function (str) {
 
 var UserQuery = function() {
   this._db = new sqlite3.Database('usersDB.db');
-  this._db.each("SELECT * FROM users", function (err, row) {
-    console.log(row);
-  });
-  this._db.each("SELECT * FROM bagged", function (err, row) {
-    console.log(row);
+  var this._db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'secret',
+    database : 'usersDB'
   });
 }
 
-UserQuery.prototype.oneById = function(username, onQueryFinished) {
-
-  var sql = "SELECT bagged.mountain_id, bagged.has_bagged " +
-    "FROM users JOIN bagged ON users.id = bagged.user_id WHERE users.username = " + quote(username);
-    console.log(sql);
+UserQuery.prototype.oneById = function(username) {
+  var sql = "SELECT users_bagged.mtn_id, users_bagged.bagged " +
+    "FROM users INNER JOIN users_bagged ON users.id = users_bagged.user_id WHERE users.username = " +
+    quote(username) + ";";
+    console.log(sql)
+  var sql = "SELECT * FROM users;"
+  console.log(sql)  
   this._db.each(sql, function (err, row) {
     console.log(row);
   });
 }
 
-UserQuery.prototype.updateBaggedList = function(userId, mtnList, onQueryFinished) {
-  MongoClient.connect(this.url, function(err, db) {
-    if (db) {
-      var collection = db.collection('users');
-      collection.findOne({ "_id": userId }, function(err, user) {
-        var userMtns = user.public.mountains;
-        for (var mtn of mtnList) {
-          for (var c = 0; c < userMtns.length; c++) {
-            if (mtn.mtn_id === userMtns[c].mtn_id) {
-              userMtns[c] = mtn;
-            }
-          }
-        }
-        collection.save(user, {w:1}, function(err, result) {
-          onQueryFinished();
-          db.close();
-        })
-      })
-    }
-  })
+UserQuery.prototype.updateBaggedList = function(userId, mtnList) {
+
 }
 
 module.exports = UserQuery;
