6e356590a13ee2c84bdf75c6e111e83362242331
siansrd <sianrobinsondavies@gmail.com>
2016-10-27 15:04:49 +0100

    Set map bounds and zoom

--- client/src/models/map_obj.js
+++ client/src/models/map_obj.js
@@ -1,12 +1,18 @@
 var MapObject = function(container) {
 
   this.map = new google.maps.Map(container, {
-    center: new google.maps.LatLng(56.87039900,-4.1988390),
+    center: new google.maps.LatLng(57.450861,-1.604004),
     zoom: 7,
     minZoom: 7,
     mapTypeId: 'terrain'
   });
 
+  const ne = new google.maps.LatLng(59.073548704841784, 2.1691826171875164);
+  const sw = new google.maps.LatLng(55.59337026438907, -7.853101562500001);
+  this.bounds = new google.maps.LatLngBounds(sw, ne);
+  
+  this.map.fitBounds(this.bounds);
+  
 }
 
 module.exports = MapObject;
