caaedc275b4ddd4afd6fca904974c907390bde28
siansrd <sianrobinsondavies@gmail.com>
2016-11-02 10:31:25 +0000

    Update meta description and welcome description

--- client/src/components/welcome.jsx
+++ client/src/components/welcome.jsx
@@ -4,7 +4,8 @@ const Welcome = function(props) {
 
   return (
     <div>
-      <p>Welcome to Munro Bagger. The weather displayed is taken from the Met Office as an averge of each day.</p>
+      <p>Munro Bagger maps all of the Munros in Scotland, displays their weather and allows you to keep track of the ones you've bagged.</p>
+      <p>The weather displayed is a daily forecast taken from the Met Office.</p>
       <p>Register to be able to track your bagged Munros</p>
     </div>
   )
