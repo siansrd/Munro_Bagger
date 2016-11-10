fe5b671ba6e90ced3edfa583bdd19f3e44ceaba5
John <easton3003@gmail.com>
2016-11-10 23:35:46 +0000

    bagged_munro db calls

--- client/src/components/mount_detail_info.jsx
+++ client/src/components/mount_detail_info.jsx
@@ -19,20 +19,20 @@ const MountDetailInfo = React.createClass({
         <h3>{this.props.focusMount.detail.name}</h3>
         <h4>({this.props.focusMount.detail.meaning})</h4>
         <div className="flex-grid">
-          <div className="grid-item">Region:</div> 
+          <div className="grid-item">Region:</div>
           <div className="grid-item">{this.props.focusMount.detail.region}</div>
-          <div className="grid-item">Height:</div> 
+          <div className="grid-item">Height:</div>
           <div className="grid-item">{this.props.focusMount.detail.height}m</div>
           <div className="grid-item">OS Grid Ref:</div>
           <div className="grid-item">{this.props.focusMount.detail.gridRef.letters} {this.props.focusMount.detail.gridRef.eastings} {this.props.focusMount.detail.gridRef.northings}</div>
-          <div className="grid-item">Lat/Lon:</div> 
+          <div className="grid-item">Lat/Lon:</div>
           <div className="grid-item">{this.props.focusMount.detail.latLng.lat} {this.props.focusMount.detail.latLng.lng}</div>
-          
-          <div className="grid-item">Weather {this.props.dayNum}:</div> 
+
+          <div className="grid-item">Weather {this.props.dayNum}:</div>
           <div className="grid-item">{description}</div>
-          <div className="grid-item">Temp:</div> 
+          <div className="grid-item">Temp:</div>
           <div className="grid-item">Max: {tempMax}&deg;C, Feels Like: {tempFL}&deg;C</div>
-          <div className="grid-item">Wind:</div> 
+          <div className="grid-item">Wind:</div>
           <div className="grid-item">{windDirection} {windSpeed}mph, Gusts: {gusts}mph</div>
         </div>
       </div>
