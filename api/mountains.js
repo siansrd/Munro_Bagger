b76f1aa0adc700288acdb71a47e13ee007726663
Phil Crooks <pacman@crooks.me.uk>
2016-11-11 14:52:06 +0000

    unused files deleted

--- api/mountains.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-var getMountains = function() {
-  var jsonMtns = fs.readFileSync(__dirname + '/mountains.json');
-  return JSON.parse(jsonMtns);
-}
-
-module.exports = getMountains;
