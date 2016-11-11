af5f984691e804158570510eff780e612318b200
Phil Crooks <pacman@crooks.me.uk>
2016-11-11 15:01:47 +0000

    mountain and user api removed from server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var app = express();
-var MountainApi = require('./api/mountain_api');
-var UserApi = require('./api/user_api')
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
 app.use(express.static('client/build'));
 
 app.listen(3000, function () {
-  new MountainApi(app);
-  new UserApi(app);
   console.log('App running on port', this.address().port);
 });
