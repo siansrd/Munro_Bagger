cb86c6ba5de8242bce94579ed42c78dec8672342
siansrd <sianrobinsondavies@gmail.com>
2016-11-11 16:26:55 +0000

    Change active forcaste button color depending whats selected

--- client/src/components/forecast.jsx
+++ client/src/components/forecast.jsx
@@ -2,18 +2,38 @@ const React = require('react');
 
 const Forecast = React.createClass ({
 
+  getInitialState: function(){
+    return {
+      selected: "0"
+    }
+  },
+
+  isFocus:function(value){
+    return ((value===this.state.selected) ? 'active':'inactive');
+  },
+
   changeForecast: function(event) {
     let dayNum = event.target.value
-    this.props.selectForecast(dayNum);
+    this.props.selectForecast(dayNum)
+    this.setState({selected: event.target.value})
   },
 
   render: function() {
     return (
       <div id="weather-buttons">
         <h4>Weather</h4>
-        <button value="0" onClick={this.changeForecast}>Today</button>
-        <button value="1" onClick={this.changeForecast}>Tomorrow</button>
-        <button value="2" onClick={this.changeForecast}>Day After</button>
+        <button 
+          className={this.isFocus("0")} 
+          value="0"
+          onClick={this.changeForecast}>Today</button>
+        <button 
+          className={this.isFocus("1")} 
+          value="1" 
+          onClick={this.changeForecast}>Tomorrow</button>
+        <button 
+          className={this.isFocus("2")} 
+          value="2" 
+          onClick={this.changeForecast}>Day After</button>
       </div>
     )
   }
