b2c1879c40e618534f7f4d5fb400dc8294d39aac
John <easton3003@gmail.com>
2016-11-14 15:36:04 +0000

    file created

---
+++ client/src/components/contact_us.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+const ContactUs = React.createClass ({
+
+  render: function() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+});
+
+module.exports = ContactUs;
