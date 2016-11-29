3af2f648d5a5516ea5670d0eedb88a13354a9e3b
Phil <pacman@crooks.me.uk>
2016-11-29 19:17:44 +0000

    started changing for...of to for loops

--- client/src/models/forecasts.js
+++ client/src/models/forecasts.js
@@ -4,9 +4,12 @@ var Forecasts = function(forecasts){
 
   this._forecasts = [];
 
-  for (var forecast of forecasts.days) {
-    this._forecasts.push(new Forecast(forecast));
+  for (let i = 0; i < forecasts.days.length; i++) {
+  	this._forecasts.push(new Forecast(forecasts.days[i]));
   }
+  // for (var forecast of forecasts.days) {
+  //   this._forecasts.push(new Forecast(forecast));
+  // }
 
   Object.defineProperty(this, "day", {
     get: function(){
--- client/src/models/mountains.js
+++ client/src/models/mountains.js
@@ -1,9 +1,9 @@
 let Mountain = require('./mountain');
 let ApiRequest = require('./api_request');
 
-const baseURL = "http://www.munrobagger.scot/";
+// const baseURL = "http://www.munrobagger.scot/";
 // const baseURL = "http://localhost:3000/";
-// const baseURL = "http://192.168.1.124:3000/";
+const baseURL = "http://192.168.1.124:3000/";
 
 var Mountains = function(){
 };
@@ -13,10 +13,14 @@ Mountains.prototype.all = function(onCompleted) {
   const apiRequest = new ApiRequest();
   apiRequest.makeGetRequest(url, null, function(status, receivedMtns) {
     const mountains = [];
-    for (let receivedMtn of receivedMtns) {
-      let mtn = new Mountain(receivedMtn);
+    for (let i = 0; i < receivedMtns.length; i++) {
+      let mtn = new Mountain(receivedMtns[i]);
       mountains.push(mtn);
     }
+    // for (let receivedMtn of receivedMtns) {
+    //   let mtn = new Mountain(receivedMtn);
+    //   mountains.push(mtn);
+    // }
     onCompleted(mountains);
   })
 };
