639872e73a08f42c28ed571ab2a1752afc379549
siansrd <sianrobinsondavies@gmail.com>
2016-12-01 18:19:42 +0000

    Further improve icon key on welcome message

--- client/src/components/welcome.jsx
+++ client/src/components/welcome.jsx
@@ -12,15 +12,15 @@ const Welcome = React.createClass ({
         <p>Munro Bagger maps all of the Munros in Scotland, displays their weather and allows you to keep track of the ones you've bagged. The weather is a daily forecast taken from the Met Office. Click the icons on the map to see more detail.</p>
 
         <div className="flex-grid">
-          <div className="grid-item-wide">Munro (Sunny/Not Sunny):</div>
-          <div className="grid-item-wide"><img className="icon" src="/public/images/mntn-sunny.png"/><img className="icon" src="/public/images/mntn-not-sunny.png"/></div>
+          <div className="grid-item-key"><img className="icon" src="/public/images/mntn-sunny.png"/><img className="icon" src="/public/images/mntn-not-sunny.png"/></div>
+          <div className="grid-item-key">- Munro (Sunny/Not Sunny)</div>
         </div>
         <p><span className="user-link" onClick={this.clickSignUp}>Register</span> to track your bagged Munros.</p>
         <div className="flex-grid">
-          <div className="grid-item-wide">Bagged (Sunny/Not Sunny):</div>
-          <div className="grid-item-wide"><img className="icon" src="/public/images/mntn-bagged-sunny.png"/><img className="icon" src="/public/images/mntn-bagged.png"/></div>
-          <div className="grid-item-wide">Not Bagged (Sunny/Not Sunny):</div>
-          <div className="grid-item-wide"><img className="icon" src="/public/images/mntn-not-bagged-sunny.png"/><img className="icon" src="/public/images/mntn-not-bagged.png"/></div>
+          <div className="grid-item-key"><img className="icon" src="/public/images/mntn-bagged-sunny.png"/><img className="icon" src="/public/images/mntn-bagged.png"/></div>
+          <div className="grid-item-key">- Bagged (Sunny/Not Sunny)</div>
+          <div className="grid-item-key"><img className="icon" src="/public/images/mntn-not-bagged-sunny.png"/><img className="icon" src="/public/images/mntn-not-bagged.png"/></div>
+          <div className="grid-item-key">- Not Bagged (Sunny/Not Sunny)</div>
         </div>
       </div>
     )
