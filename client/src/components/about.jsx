7673916dd1e0b4dc7990bb75694d302a4b9d6195
siansrd <sianrobinsondavies@gmail.com>
2016-12-12 22:52:17 +0000

    Add symbol thing to sian's name

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -7,7 +7,7 @@ const About = React.createClass ({
       <div>
         <p>Munro Bagger maps all of the Munros in Scotland, displays their weather and allows you to keep track of the ones you've bagged. The weather is a daily forecast taken from the Met Office.</p>
         <p>
-        Munro Bagger was created as part of a <a className="user-link" target="_blank" href="https://codeclan.com/">CodeClan</a> project by Phil Crooks, John Easton & Sian Robinson Davies from Cohort 6.
+        Munro Bagger was created as part of a <a className="user-link" target="_blank" href="https://codeclan.com/">CodeClan</a> project by Phil Crooks, John Easton & Siân Robinson Davies from Cohort 6.
         </p>
         <p>
           If you have any queries or suggestions, please get in touch on twitter!
