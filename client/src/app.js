b297f6b0f1d73e7707e5a8393df13199fafa2544
siansrd <sianrobinsondavies@gmail.com>
2016-12-18 11:23:03 +0000

    Remove flexibility

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const MapObject = require('./views/map.js')
-const UI = require('./components/ui')
-const flexibility = require('./flexibility')
+const MapObject = require('./views/map.js');
+const UI = require('./components/ui');
 
 window.onload = function(){
   const mapObj = new MapObject(document.getElementById('map'));
